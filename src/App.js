ff3e42085f88ebd80a21394348d3c0c051bf703d
Elisa Bourgeois <elisabourgeois.eb@gmail.com>
2018-12-26 11:32:30 +0100

    First part of the top AppBar (caregivers part)

--- src/App.js
+++ src/App.js
@@ -1,31 +1,63 @@
 import React from 'react';
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard';
 
-import RemainingComponent from './components/RemainingComponent';
+// import RemainingComponent from './components/RemainingComponent';
+import AppBarGlobal from './components/AppBarGlobal';
 import Calendar from './components/Calendar/Calendar';
 
 import CaregiversForm from './components/Caregiver/CaregiverForm';
 import Contact from './components/Contacts/Contact';
 
 import './App.css';
+import 'typeface-roboto';
+
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true,
+  },
+  palette: {
+    primary: {
+      main: '#65CDE2',
+      contrastText: '#fff',
+    },
+    secondary: {
+      main: '#FC8F72',
+      contrastText: '#fff',
+    },
+  },
+  props: {
+    // Name of the component ⚛️
+    MuiAppBar: {
+      // The properties to apply
+      elevation: 0, // No more ripple, on the whole application 💣!
+    },
+  },
+  overrides: {
+    MuiAppBar: { // Name of the component ⚛️ / style sheet
+      root: { // Name of the rule
+        padding: 0, // Some CSS
+      },
+    },
+  },
+});
 
 const App = () => (
-  <div className="App">
-    <BrowserRouter>
+  <MuiThemeProvider theme={theme}>
+    <div className="App">
       <div>
-        <RemainingComponent />
-        <Switch>
-          <Route exact path="/" component={Dashboard} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/création" component={CaregiversForm} />
-          <Route path="/calendrier" component={Calendar} />
-          {/* <Route path='/suivi' component={FollowedUp} /> */}
-          {/* <Route path='/mon_compte' component={Account} /> */}
-        </Switch>
+        {/* <RemainingComponent /> */}
+        <AppBarGlobal />
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/création" component={CaregiversForm} />
+        <Route path="/calendrier" component={Calendar} />
+        {/* <Route path='/suivi' component={FollowedUp} /> */}
+        {/* <Route path='/mon_compte' component={Account} /> */}
       </div>
-    </BrowserRouter>
-  </div>
+    </div>
+  </MuiThemeProvider>
 );
 
 
---
+++ src/components/AppBarGlobal.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import AppBarCaregiver from './AppBarCaregiver';
+
+class AppBarGlobal extends Component {
+  render() {
+    return (
+      <div className="AppBarGlobal">
+        <AppBarCaregiver />
+      </div>
+    )
+  }
+}
+
+export default AppBarGlobal;
