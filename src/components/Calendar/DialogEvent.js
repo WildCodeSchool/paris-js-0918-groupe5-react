dc74df03e347e572ae1dcff0f1c5681305f56c30
Widaad Barreto <widaad.barreto@gmail.com>
2019-01-04 09:38:09 +0100

    including formredux

--- src/components/Calendar/DialogEvent.js
+++ src/components/Calendar/DialogEvent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
+import { formValueSelector, reduxForm, Field } from 'redux-form';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -10,12 +11,15 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
 import { recordName } from '../../actions/eventActions';
+import { renderTextField } from '../reduxFormElements';
 
 import SwitchLabel from './SwitchLabels';
 import SimpleSelect from './SimpleSelect';
 import SimpleSelectAddress from './SimpleSelectAddress';
 import DateAndTimePickers from './DateAndTimePickers';
 // import { assertExpressionStatement } from 'babel-types';
+
+// valide allows to detect errors (see reduxFormElements.js)
 
 class DialogEvent extends Component {
       state = {
@@ -34,6 +38,26 @@ class DialogEvent extends Component {
         // mood: '',
         // status: false
       }
+
+      validate = (values) => {
+        const errors = {};
+        const requiredFields = [
+          'firstName',
+          'lastName',
+        ];
+        requiredFields.forEach((field) => {
+          if (!values[field]) {
+            errors[field] = 'Champs requis';
+          }
+        });
+        // if (
+        //   values.email &&
+        //   !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+        // ) {
+        //   errors.email = 'Invalid email address'
+        // }
+        return errors;
+      };
 
       handleClose = () => {
         this.props.onOpen();
