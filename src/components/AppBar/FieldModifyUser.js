28466e4d834517b95a1bf1968713c984ec4c4ae4
Widaad Barreto <widaad.barreto@gmail.com>
2019-01-15 11:09:18 +0100

    one field to modify name is ok and close on clicking fermer

---
+++ src/components/AppBar/FieldModifyUser.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+class MyAccountModale extends React.Component {
+  render() {
+    const { openField, onClose } = this.props;
+    return (
+      <div>
+        <Dialog
+          open={openField}
+          onClose={this.handleCloseFieldModifyAccount}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Mon compte</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Vous trouverez ci-dessous vos informations personnelles.
+              <br />
+              Si vous souhaitez les modifier, nous vous invitons à cliquer sur le bouton "Modifier".
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Email Address"
+              type="email"
+              fullWidth
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={onClose} color="primary">
+              Fermer
+            </Button>
+						<Button color="primary">
+              Enregistrer
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+export default MyAccountModale;
--- src/components/AppBar/MyAccountModale.js
+++ src/components/AppBar/MyAccountModale.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+// import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import { IconButton } from '@material-ui/core';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -8,10 +8,25 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Icons from '../Icons';
+import FieldModifyUser from './FieldModifyUser';
 
 class MyAccountModale extends React.Component {
+  state = {
+    openFieldModifyAccount: false,
+  }
+
+  openFieldModifyAccount = () => {
+    this.setState({ openFieldModifyAccount: true });
+  }
+
+  handleCloseFieldModifyAccount = () => {
+    this.setState({ openFieldModifyAccount: false });
+  };
+
   render() {
     const { open, onClose } = this.props;
+    const { openFieldModifyAccount } = this.state;
+    console.log(openFieldModifyAccount);
     return (
       <div>
         <Dialog
@@ -22,7 +37,7 @@ class MyAccountModale extends React.Component {
           <DialogTitle id="form-dialog-title">Mon compte</DialogTitle>
           <DialogContent>
             <DialogContentText>
-              Vous trouverez ci-dessous vos informations personnelles. 
+              Vous trouverez ci-dessous vos informations personnelles.
               <br />
               Si vous souhaitez les modifier, nous vous invitons à cliquer sur le bouton "Modifier".
             </DialogContentText>
@@ -37,7 +52,7 @@ class MyAccountModale extends React.Component {
             <h4>Nom</h4>
             <p>
               Jolivet Karine
-              <IconButton>
+              <IconButton onClick={this.openFieldModifyAccount}>
                 <Icons name="EditIcon" />
               </IconButton>
             </p>
@@ -78,6 +93,7 @@ class MyAccountModale extends React.Component {
             </Button>
           </DialogActions>
         </Dialog>
+        <FieldModifyUser openField={openFieldModifyAccount} onClose={this.handleCloseFieldModifyAccount} />
       </div>
     );
   }
